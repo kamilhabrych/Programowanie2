a658d5e43b7cd93eaf90e4411bf83bdb52783e6e
Kamil Habrych <habrychkamil@gmail.com>
2020-03-27 14:37:41 +0100

    parse

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Programowanie2
 {
@@ -6,7 +7,15 @@ namespace Programowanie2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double result = 0.0;
+            Programowanie2.ReversePolishNotation rpn = new Programowanie2.ReversePolishNotation();
+            rpn.Parse("3+4*2/(1-5)^2^3");
+            result = rpn.Evaluate();
+            Console.WriteLine("orig:   {0}", rpn.OriginalExpression);
+            Console.WriteLine("tran:   {0}", rpn.TransitionExpression);
+            Console.WriteLine("post:   {0}", rpn.PostfixExpression);
+            Console.WriteLine("result: {0}", result);
+            Console.ReadLine();
         }
     }
 }
